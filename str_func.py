efb8184093541c63e904ab5db1e0d6871658d3fa
Denis_L <denis88lyapin@gmail.com>
2023-04-20 19:17:34 +0300

    fixed letters_upper, added nef feature

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def letters_upper(letters):
+    """возвращает заглавные буквы"""
     return letters.upper()
+
+def  first_letter_title(letters):
+    """возвращает строку с заглавное буквой"""
+    return letters.title()
